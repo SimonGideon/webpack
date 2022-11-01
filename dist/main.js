9a3fd8772b14ed25faae5af86681d86a24dfdb9a
SimonGideon <simongideon918@gmail.com>
2022-11-01 20:43:37 +0300

    Refresher

--- dist/main.js
+++ dist/main.js
@@ -26,7 +26,7 @@ eval("/* module decorator */ module = __webpack_require__.nmd(module);\nvar __WE
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\n function component() {\n   const element = document.createElement('div');\n\n  // Lodash, currently included via a script, is required for this line to work\n  // Lodash, now imported by this script\n   element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Hello', 'webpack'], ' ');\n\n   return element;\n }\n\n document.body.appendChild(component());\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\n\n function component() {\n   const element = document.createElement('div');\n\n  // Lodash, now imported by this script\n   element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Hello', 'webpack'], ' ');\n\n   return element;\n }\n\n document.body.appendChild(component());\n\n//# sourceURL=webpack://webpack/./src/index.js?");
 
 /***/ })
 
