9917170ed015aa6601c14854076a48db30942499
SimonGideon <simongideon918@gmail.com>
2022-11-02 01:31:18 +0300

    ADD linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,44 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/app.js')
-    },
-    output: {
-        filename: '[name].bundle.js',
-    },
-    devServer: {
-        static: "./dist",
-        open: true
-    },
+  entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Weibeans is awesome"
-        })
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weibeans is awesome',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          }
-        ]
-      }
+  },
 };
