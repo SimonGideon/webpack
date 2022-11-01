780c1297415554c3615d7c986d06920d4379f658
SimonGideon <simongideon918@gmail.com>
2022-11-02 01:20:30 +0300

    Refactor template with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: {
+        main: path.resolve(__dirname, './src/app.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+    },
+    devServer: {
+        static: "./dist",
+        open: true
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Weibeans is awesome"
+        })
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          }
+        ]
+      }
 };
-
-module.exports = {
-    mode: 'development',
-  };
